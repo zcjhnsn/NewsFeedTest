da82ce1e2c3aea1253db07314355b7c34f6276e8
Zac Johnson <zac@lightningkite.com>
2020-01-02 13:57:19 -0700

    Initial Commit

---
+++ NewsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsTest
+//
+//  Created by Zac Johnson on 1/2/20.
+//  Copyright © 2020 Zac Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
