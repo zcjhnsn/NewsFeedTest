98b8117a486b67bdca31b59e8ffe4c7d850eccc4
Zac Johnson <zac@lightningkite.com>
2020-01-31 15:29:00 -0700

    adjusted title height

--- NewsTest/Sections/SectionTitleSection.swift
+++ NewsTest/Sections/SectionTitleSection.swift
@@ -21,7 +21,7 @@ struct SectionTitleSection: Section {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(40))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
         let section = NSCollectionLayoutSection(group: group)
